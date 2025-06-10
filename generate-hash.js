b3b47075dcf48f1b89f8c7cd170bdfa3a2e0394c
Fari Noveri <cahayalunamaharani1@gmail.com>
2025-06-10 09:33:17 +0700

    Update .gitignore and remove sensitive files from tracking

--- generate-hash.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const crypto = require('crypto');
-
-const data = fs.readFileSync('script.js');
-const hash = crypto.createHash('sha256').update(data).digest('hex');
-console.log(hash);
